ab12d566a7519ceb2187a21fd4e4d0d2ff300a2e
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-03 12:08:29 +0300

    feat: :zap: load planets data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,37 +1,52 @@
-const parse = require("csv-parse");
 const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
+
+// Helpers
+const parser = parse({
+  // options
+  comment: "#",
+  columns: true, // now each row will be treaded as a js-object (key-value)
+});
+const isHabitable = (planet) => {
+  return (
+    planet.koi_disposition === "CONFIRMED" &&
+    planet.koi_insol > 0.36 &&
+    planet.koi_insol < 1.11 &&
+    planet.koi_prad < 1.6
+  );
+};
 
 const habitablePlanets = [];
 
-function isHabitablePlanet(planet) {
-  return (
-    planet["koi_disposition"] === "confirmed" &&
-    planet["koi_insol"] > 0.36 &&
-    planet["koi_insol"] < 1.11 &&
-    planet["koi_prad"] < 1.6
-  );
+function loadPlanetsData() {
+  // createReadStream --> deal with each row (stream) alone so that we can deal with scalable size --> using streams to handle data as it comes-in
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(parser)
+      .on("data", (data) => {
+        if (isHabitable(data)) {
+          habitablePlanets.push(data); // results are RAW-Buffers of Bytes so we need to parse it
+        }
+      })
+      .on("err", () => {
+        console.log("found an error 🤦‍♀️");
+        reject(err);
+      })
+      .on("end", () => {
+        // console.log(
+        //   `found ${habitablePlanets.length} planets that are habitable they are : ${planetNames}`
+        // );
+        console.log("done processing files");
+
+        resolve();
+      });
+  });
 }
 
-fs.createReadStream("3.1 kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(habitablePlanets);
-    console.log(`${habitablePlanets.length} habitable planet`);
-  });
-
 module.exports = {
+  loadPlanetsData,
   planets: habitablePlanets,
 };
