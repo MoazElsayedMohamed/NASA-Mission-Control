f734d80d94af45a6bc669af0cb4101280c27d579
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-03 21:21:39 +0300

    feat: :sparkles: validate post launches for post requests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,8 +6,24 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
 
   launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "invalid launch date",
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
