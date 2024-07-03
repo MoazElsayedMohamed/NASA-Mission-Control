bf5d6852281aa9a9c65b94022210ec668d73930f
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-03 11:09:28 +0300

    feat: :sparkles: install csv-parse and get planets data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,37 @@
-const planets = [];
+const parse = require("csv-parse");
+const fs = require("fs");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "confirmed" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("3.1 kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(habitablePlanets);
+    console.log(`${habitablePlanets.length} habitable planet`);
+  });
+
+module.exports = {
+  planets: habitablePlanets,
+};
