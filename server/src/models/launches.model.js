a8797561e459fd5bb38b865dc928a089fe3c96e9
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-03 17:39:26 +0300

    feat: :sparkles: post (create) launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -17,6 +19,20 @@ function getAllLaunches() {
   return launches;
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customer: ["Zero to Mystery", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const { httpGetAllPlanets } = require("./planets.controller");
 const planetsRouter = express.Router();
-planetsRouter.get("/planets", httpGetAllPlanets);
+planetsRouter.get("/", httpGetAllPlanets);
 module.exports = planetsRouter;
