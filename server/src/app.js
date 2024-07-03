be99857f3948bc0189001ad010c44f02f5e772eb
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-03 13:15:26 +0300

    feat: :zap: automating full stack application with npm

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
@@ -9,6 +10,12 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public"));
+});
 
 module.exports = app;
