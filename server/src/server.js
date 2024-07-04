127ce42dfd34c49d5b6bebc1ad89a197c346764c
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-07-04 21:18:58 +0300

    feat: :zap: connect our server to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,25 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:T6rBEQ6B2OFPV5NC@cluster0.ekup6bo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
@@ -17,3 +34,5 @@ async function startServer() {
 }
 
 startServer();
+
+// moongodb password for database access : T6rBEQ6B2OFPV5NC
